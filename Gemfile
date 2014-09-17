6808fb4e669e8117f3ae47fe0bd0bdeda66e7e48
Ko Miyake <dispensermk@gmail.com>
2014-09-17 14:40:45 +0900

    Make a basic User model (including secure passwords)

--- Gemfile
+++ Gemfile
@@ -15,6 +15,7 @@ group :test do
     gem 'selenium-webdriver'
 end
 
+gem 'bcrypt-ruby', '3.1.2'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.3'
 # Use Uglifier as compressor for JavaScript assets
--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140906142828) do
+ActiveRecord::Schema.define(version: 20140917050504) do
 
-  create_table "purchase_requests", force: true do |t|
-    t.string   "title"
-    t.string   "isbn"
-    t.string   "comment"
+  create_table "users", force: true do |t|
     t.string   "name"
+    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
